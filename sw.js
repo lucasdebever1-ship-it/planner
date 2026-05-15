63e550e6c3d565339656d4d6c49b86b47b0bcdee
Dirk <lucas.de.bever1@gmail.com>
2026-05-15 13:36:26 +0200

    PWA toegevoegd: manifest, service worker en app icoon

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE = 'struct-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/overzicht.html',
+  '/week.html',
+  '/vakken.html',
+  '/instellingen.html',
+  '/onboarding.html',
+  '/manifest.json',
+  '/icon.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      });
+      return cached || network;
+    })
+  );
+});
